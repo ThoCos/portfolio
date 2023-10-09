d4462561cf547718ecf99f5a2cfecb383bdd0479
ThoCos <144100931+ThoCos@users.noreply.github.com>
2023-10-09 10:20:14 +0200

    Add files via upload

---
+++ src/components/Nav.vue
@@ -0,0 +1,30 @@
+<script setup>
+import NavLink from './NavLink.vue';
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+    <p><img src="https://thomascosin.be/img/logo_nom.png" width=70></p>
+      <RouterLink class="navbar-brand" to="/"> Accueil</RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <NavLink url="/" text="Accueil" />
+          <NavLink url="/about" text="À propos" />
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="display-4">Portfolio</h1>
+      <p class="lead">
+        Bienvenue sur mon portfolio !
+      </p>
+      <RouterLink to="/about" class="btn btn-primary btn-lg"
+        >Me contacter</RouterLink
+      >
+    </div>
+  </div>
+</template>
