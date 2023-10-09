3028ceaee239b8e7a8c921a68aef95e6988867af
ThoCos <thomascosinifogmail.com>
2023-10-09 11:58:23 +0200

    wip

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer mt-auto py-3 bg-body-tertiary">
     <div class="container">
-      <span class="text-body-secondary">Place sticky footer content here.</span>
+      <span class="text-body-secondary">Mentions légales | infos de copyrights</span>
     </div>
   </footer>
 </template>
